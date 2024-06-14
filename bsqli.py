e7baa4cf661033262276156e3c7e6a38262903ce
Wh0l5Th3R00t <155916762+k45t0@users.noreply.github.com>
2024-06-13 23:53:02 -0300

    Update bsqli.py

--- bsqli.py
+++ bsqli.py
@@ -1,11 +1,10 @@
-#CODED BY @wh0l5th3r00t
+#CODED BY @whol5th3r00t
 
 import argparse
 import requests
 import time
 import os
 from colorama import Fore, Style, init
-from threading import Thread, BoundedSemaphore
 
 init(autoreset=True)
 
@@ -29,15 +28,15 @@ def load_payloads(server, level):
 def test_vulnerability(url, payload):
     start_time = time.time()
     try:
-        response = requests.get(url + payload, timeout=10)
+        response = requests.get(url + payload, timeout=20)
         elapsed_time = time.time() - start_time
-        if elapsed_time >= 5:
+        if 10 <= elapsed_time < 20:
             return True, elapsed_time
         else:
             return False, elapsed_time
     except requests.RequestException:
         elapsed_time = time.time() - start_time
-        if elapsed_time >= 5:
+        if 10 <= elapsed_time < 20:
             return True, elapsed_time
         return False, elapsed_time
 
@@ -56,28 +55,37 @@ def identify_server(vulnerable_payload):
                 return server
     return "Unknown"
 
-def worker(url, payloads, server, semaphore):
-    with semaphore:
+def worker(url, payloads, server, output_file):
+    with open(output_file, 'a') as out_file:
         for payload in payloads:
             vulnerable, response_time = test_vulnerability(url, payload)
             if vulnerable:
                 if server:
-                    print(f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url + payload}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}POSSIBLE SERVER: {server}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}")
+                    result = f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url + payload}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}POSSIBLE SERVER: {server}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}"
                 else:
                     detected_server = identify_server(payload)
-                    print(f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url + payload}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}POSSIBLE SERVER: {detected_server}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}")
-                break
+                    result = f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url + payload}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}POSSIBLE SERVER: {detected_server}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}"
+                print(result)
+                out_file.write(f"{url + payload}\n")
+                return True
             else:
-                print(f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.RED}NOT VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {url + payload} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}")
+                result = f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.RED}NOT VULNERABLE{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url + payload}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}RESPONSE TIME: {response_time:.2f}s{Style.RESET_ALL}"
+                print(result)
+    return False
+
+def process_url(url, payloads, server, output_file):
+    url = url.strip()
+    vulnerable = worker(url, payloads, server, output_file)
+    return url, vulnerable
 
 def main():
     parser = argparse.ArgumentParser(description='Blind SQLi | CODER @wh0l5th3r00t | V2')
     parser.add_argument('-u', '--url', type=str, help='Target URL')
     parser.add_argument('-l', '--list', type=str, help='List of URLs')
-    parser.add_argument('-p', '--payload', type=str, help='Payload list')
+    parser.add_argument('-p', '--payload', type=str, help='Payload list', required=True)
     parser.add_argument('-s', '--server', type=str, choices=["MySQL", "Microsoft SQL Server", "Postgresql", "Oracle"], help='Database server')
-    parser.add_argument('-L', '--level', type=int, choices=[1, 2], default=1, help='Payload level')
-    parser.add_argument('-t', '--threads', type=int, default=10, help='Number of threads')
+    parser.add_argument('-L', '--level', type=int, choices=[1, 2, 3], default=1, help='Payload level')
+    parser.add_argument('-o', '--output', type=str, help='Output file', required=True)
 
     args = parser.parse_args()
     
@@ -91,30 +99,23 @@ def main():
     print(banner)
 
     urls = []
-    if args.url:
+    if (args.url):
         urls.append(args.url)
-    elif args.list:
+    elif (args.list):
         with open(args.list, 'r') as file:
             urls = file.readlines()
 
     payloads = []
-    if args.payload:
+    if (args.payload):
         with open(args.payload, 'r') as file:
             payloads = file.readlines()
     else:
         payloads = load_payloads(args.server, args.level)
 
-    semaphore = BoundedSemaphore(args.threads)
-    threads = []
-
     for url in urls:
-        url = url.strip()
-        thread = Thread(target=worker, args=(url, payloads, args.server, semaphore))
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+        url, vulnerable = process_url(url, payloads, args.server, args.output)
+        if vulnerable:
+            print(f"{Fore.YELLOW}[{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}SKIPPING{Style.RESET_ALL}{Fore.YELLOW}]{Style.RESET_ALL} {Fore.YELLOW}-{Style.RESET_ALL} {Fore.CYAN}{url}{Style.RESET_ALL} {Fore.YELLOW}|{Style.RESET_ALL} {Fore.LIGHTMAGENTA_EX}ALREADY VULNERABLE{Style.RESET_ALL}")
 
 if __name__ == '__main__':
     main()
